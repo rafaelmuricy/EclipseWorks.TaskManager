e67a6d883dc1eef0467021f57988feaa95ece964
rafaelmuricy <rafael_muricy@hotmail.com>
2025-04-15 18:07:34 -0300

    Add project files.

---
+++ EclipseWorks.TaskManager.Servico/Model/ProjetoModel.cs
@@ -0,0 +1,9 @@
+﻿namespace EclipseWorks.TaskManager.Servico.Model
+{
+    public class ProjetoModel
+    {
+        public int IdProjeto { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public List<TarefaModel>? Tarefas { get; set; }
+    }
+}
---
+++ EclipseWorks.TaskManager.Servico/Model/TarefaModel.cs
@@ -0,0 +1,35 @@
+﻿namespace EclipseWorks.TaskManager.Servico.Model
+{
+    public class TarefaModel
+    {
+        public int Id { get; set; }
+        public int IdProjeto { get; set; }
+        public int IdUsuario { get; set; }
+        public string Titulo { get; set; } = string.Empty;
+        public string Descricao { get; set; } = string.Empty;
+        public DateTime DataVencimento { get; set; }
+
+        public PrioridadeTarefa Prioridade { get; set; }
+        public StatusTarefa Status { get; set; }
+
+        public enum PrioridadeTarefa
+        {
+            Baixa,
+            Media,
+            Alta
+        }
+        public enum StatusTarefa
+        {
+            Pendente,
+            EmAndamento,
+            Concluida
+        }
+    }
+
+    public class TarefaComentario
+    {
+        public int IdTarefa { get; set; }
+        public int IdUsuario { get; set; }
+        public string Comentario { get; set; } = string.Empty;
+    }
+}
